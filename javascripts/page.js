37420d42615f7719f915d68ee18b18f72ed96e4e
Cat Chen <cathsfz@gmail.com>
2011-01-05 23:50:59 +0800

    added Google Analytics

--- javascripts/page.js
+++ javascripts/page.js
@@ -69,3 +69,13 @@ for (var i = 0; i < codes.length; i++) {
 SyntaxHighlighter.config.tagName = 'code';
 SyntaxHighlighter.all()
 
+/* Google Analytics */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-347601-11']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
